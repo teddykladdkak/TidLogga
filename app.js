159ce9032a4d66b1c8e5bf4e61f7b43fc0b0a516
Mattias Lidbeck <teddykladdkak@gmail.com>
2018-01-26 09:12:09 +0100

    Buss wss

--- app.js
+++ app.js
@@ -103,6 +103,7 @@ function makedirektory(projekt, vgrid){
 
 // Loading socket.io
 var io = require('socket.io').listen(server);
+	io.set('transports', ['polling', 'websocket']);
 io.sockets.on('connection', function (socket, username) {
 	// When the client connects, they are sent a message
 	socket.emit('message', 'You are connected!');
